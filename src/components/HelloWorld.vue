a06d71d9670d5ef7876174ed98c1209339cad3f1
kaan <kaandesu00@gmail.com>
2023-04-23 19:14:04 +0200

    update: live demo
    Closes #9

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,7 +10,9 @@ defineProps<{
     <h3>
       You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a> using the <br>
+      <a href="https://github.com/kaandesu/vite-vue-ultimate-starter" target="_blank"
+        rel="noopener">vite-vue-ultimate-starter</a> template! What's next?
     </h3>
   </div>
 </template>
--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -16,7 +16,9 @@ import SupportIcon from './icons/IconSupport.vue'
 
     Vue’s
     <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
+    provides you with all information you need to get started. If you need more information about this template
+    and it's features, check out the <a href="https://github.com/kaandesu/vite-vue-ultimate-starter" target="_blank"
+      rel="noopener">vite-vue-ultimate-starter</a>.
   </WelcomeItem>
 
   <WelcomeItem>
@@ -31,12 +33,8 @@ import SupportIcon from './icons/IconSupport.vue'
     <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a> +
     <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If
     you need to test your components and web pages, check out
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
-    <a href="https://on.cypress.io/component" target="_blank">Cypress Component Testing</a>.
-
-    <br />
-
-    More instructions are available in <code>README.md</code>.
+    <a href="https://vitest.dev/" target="_blank" rel="noopener">Vitest</a> and
+    <a href="https://playwright.dev/" target="_blank">Playwright Testing</a>.
   </WelcomeItem>
 
   <WelcomeItem>
@@ -46,13 +44,12 @@ import SupportIcon from './icons/IconSupport.vue'
     <template #heading>Ecosystem</template>
 
     Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
+    <a href="https://vite-pwa-org.netlify.app/" target="_blank" rel="noopener">Vite PWA</a>,
     <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
+    <a href="https://tailwindcss.com/" target="_blank" rel="noopener">Tailwind CSS</a>,
+    <a href="https://daisyui.com/" target="_blank" rel="noopener">DaisyUI</a>,
     <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
-    you need more resources, we suggest paying
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
+    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>.
   </WelcomeItem>
 
   <WelcomeItem>
