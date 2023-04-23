833f3048c3b0ace02b253b74447f6cbe674a3c02
kaan <kaandesu00@gmail.com>
2023-04-23 10:29:58 +0200

    fix: CI workflows

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,7 +37,7 @@ const config: PlaywrightTestConfig = {
     baseURL: 'http://localhost:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
 
     /* Only on CI systems run the tests headless */
     headless: !!process.env.CI
